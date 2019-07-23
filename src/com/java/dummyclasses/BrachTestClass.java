648484c5aa2a855543dd5058c2854ca337ceaee0
shahinshaiek <shahin.manvi095@gmail.com>
2019-07-23 21:43:16 +0530

    This is the brach test commit from eclipse

---
+++ src/com/java/dummyclasses/BrachTestClass.java
@@ -0,0 +1,7 @@
+package com.java.dummyclasses;
+
+public class BrachTestClass {
+public static void main(String[] args) {
+	System.out.println("branch is added in local to work with eclipse");
+}
+}
